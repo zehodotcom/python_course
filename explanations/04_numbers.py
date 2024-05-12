adac169162d805fb2cf923d386d7ad070650df68
Angel Garcia <agmartin79@gmail.com>
2024-05-12 09:51:03 +0200

    Added 04 - Numbers

---
+++ explanations/04_numbers.py
@@ -0,0 +1,27 @@
+number = 6  # integer
+decimal = 5.82  # float
+imaginary = 4 + 4j  # complex number
+
+# Incrementing 'number' by 3
+number += 3
+
+# Printing basic arithmetic operations
+print(1 + 2)  # Addition
+print(3 - 2)  # Subtraction
+print(3 * 2)  # Multiplication
+print(3 / 2)  # Division
+print(3 // 2)  # Integer division (result without decimals)
+print(3 % 2)  # Module (remainder of division)
+print(5**7)  # Exponentiation (5 raised to the power of 7)
+
+# Common functionalities when working with numbers in Python:
+print(abs(-5))  # Absolute value
+print(round(2.567))  # Rounding to the nearest integer
+print(max(3, 7, 1))  # Print maximum value
+print(min(3, 7, 1))  # Print minimum value
+print(complex(28, 6))  # Creation of a complex number
+
+# Converting between numerical bases
+print(bin(5))  # Binary representation of 5
+print(oct(5))  # Octal representation of 5
+print(hex(16))  # Hexadecimal representation of 16
