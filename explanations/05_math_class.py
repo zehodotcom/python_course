11f5b38bf07578f97586848a3952a4b23baa609f
Angel Garcia <agmartin79@gmail.com>
2024-05-13 19:37:09 +0200

    Added 05 - Math Class

---
+++ explanations/05_math_class.py
@@ -0,0 +1,19 @@
+import math  # Module math.py with specific functions
+
+# The math module in Python is of significant importance as it provides a wide range of mathematical functions and constants, allowing developers to perform complex mathematical operations with ease.
+
+# Rounding numbers
+print(round(6.6))  # Rounds 6.6 to the nearest integer
+print(round(4.2))  # Rounds 4.2 to the nearest integer
+print(round(9.5))  # Rounds 9.5 to the nearest integer
+
+# Getting absolute values
+print(abs(-52))  # Returns the absolute value of -52
+print(abs(91))  # Returns the absolute value of 91
+
+# Math module functionalities
+print(math.ceil(1.1))  # Rounds up to the nearest integer
+print(math.floor(1.99))  # Rounds down to the nearest integer
+print(math.isnan(4.5))  # Checks if 4.5 is NaN - Not a Number
+print(math.pow(10, 3))  # Raises 10 to the power of 3
+print(math.sqrt(9))  # Returns the square root of 9
