8c2a0a8eff85168d732f013e919f46e1eba0a1d0
Angel Garcia <agmartin79@gmail.com>
2024-05-14 19:56:50 +0200

    Added 06 - Comparison Operators

---
+++ explanations/06_comparison_operators.py
@@ -0,0 +1,35 @@
+# Comparison operators in Python are symbols used to compare two values and determine the relationship between them.
+# They allow you to check if one value is equal, greater than, less than, or not equal to another value.
+
+# Comparing if 1 is greater than 2
+print(1 > 2)  # greater than
+
+# Comparing if 1 is less than 2
+print(1 < 2)  # less than
+
+# Comparing if 1 is less than or equal to 2
+print(1 <= 2)  # less than or equal to
+
+# Comparing if 1 is greater than or equal to 2
+print(1 >= 2)  # greater than or equal to
+
+# Comparing if 1 is greater than or equal to 2 (repetition)
+print(1 >= 2)
+
+# Comparing if 7 is equal to 7
+print(7 == 7)  # equal to
+
+# Comparing if 24 is equal to 42
+print(24 == 42)
+
+# Comparing if 3 is equal to 3
+print(3 == 3)
+
+# Comparing if 24 is equal to "42"
+print(24 == "42")
+
+# Comparing if 29 is not equal to 29
+print(29 != 29)  # not equal to
+
+# Comparing if 26 is not equal to "26"
+print(26 != "26")
