59578daa9115e2714bfe2680176efc5451d3bc70
Angel Garcia <agmartin79@gmail.com>
2024-05-15 20:03:49 +0200

    more explanation

--- explanations/07_if_else.py
+++ explanations/07_if_else.py
@@ -1,3 +1,9 @@
+# IF helps your program make decisions based on conditions,
+# ELIF lets you check for more conditions if the first one isn't true,
+# ELSE gives an alternative if the condition isn’t met
+# The order of the IF and ELIF is very important, because once you agree to enter an IF you will no longer enter the ELIF.
+# ELIF is not used as much as the IF and ELSE.
+
 hour = 14
 
 # Check the time of the day
