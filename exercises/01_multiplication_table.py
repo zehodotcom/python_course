b2dd6beaf1ca84a28bb22b9ab7f0f4159b0245ae
Angel Garcia <agmartin79@gmail.com>
2024-05-22 23:56:58 +0200

    Exercise: Multiplication Table

---
+++ exercises/01_multiplication_table.py
@@ -0,0 +1,7 @@
+# Get the number from the user
+number = int(input("Enter a number: "))
+
+# Print the multiplication table for the entered number
+print(f"Multiplication table for {number}:\n")
+for i in range(1, 11):
+    print(f"{number} x {i} = {number * i}")
