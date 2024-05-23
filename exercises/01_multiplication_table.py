1c83f1eb8a49f67fd52cc81340b921e98f32c2ff
Angel Garcia <agmartin79@gmail.com>
2024-05-23 18:42:41 +0200

    Added 10- While

--- exercises/01_multiplication_table.py
+++ exercises/01_multiplication_table.py
@@ -1,5 +1,5 @@
 # Get the number from the user
-number = int(input("Enter a number: "))
+number = int(input("Enter a number: \n"))
 
 # Print the multiplication table for the entered number
 print(f"Multiplication table for {number}:\n")
---
+++ explanations/10_while.py
@@ -0,0 +1,7 @@
+# A while loop in Python repeatedly executes a block of code as long as a specified condition is True.
+# It is useful for scenarios where the number of iterations is not known beforehand and depends on dynamic conditions
+
+count = 0
+while count < 5:
+    print("Count is:", count)
+    count += 1
