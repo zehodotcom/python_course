5631fb5193e660d20274a460a703940a3840caec
Angel Garcia <agmartin79@gmail.com>
2024-06-27 17:13:27 +0200

    Added 15 - Lists

---
+++ explanations/15_lists.py
@@ -0,0 +1,11 @@
+# Lists in Python are ordered collections of items, which can include a mix of data types such as numbers, strings, and even other lists
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]  # List of integers
+letters = ["a", "b", "c", "d", "e", "f", "g", "h"]  # List of single character strings
+words = ["Monday", "Tuesday", "Wednesday"]  # List of day names as strings
+booleans = [True, False, False, True]  # List of boolean values
+matrices = [[0, 1], [1, 1]]  # List of lists, representing a matrix
+zeros = [0] * 10  # List of ten zeros, using multiplication for repetition
+alphanumerics = numbers + letters  # Concatenation of numbers and letters lists
+range_list = list(range(1, 11))  # List of integers from 1 to 10, using range()
+chars = list("hello world")  # List of characters from the string "hello world"
