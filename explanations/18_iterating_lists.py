bbeb8328ae0615f22e1ef1ffc5df4395e35ca61c
Angel Garcia <agmartin79@gmail.com>
2024-07-03 20:58:15 +0200

    18 - iterating lists

---
+++ explanations/18_iterating_lists.py
@@ -0,0 +1,19 @@
+animals = [
+    "dog",
+    "cat",
+    "bird",
+    "elephant",
+    "tiger",
+    "snail",
+    "horse",
+    "shark",
+    "eagle",
+]
+
+# Iterate over the list and print each animal name
+for animal in animals:
+    print(animal)
+
+# Using enumerate to print each animal with its index as a tuple
+for index, animal in enumerate(animals):
+    print(index, animal)
